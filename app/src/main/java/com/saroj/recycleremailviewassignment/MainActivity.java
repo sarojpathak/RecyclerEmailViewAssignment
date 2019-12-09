b8839ca2b79fd7b6dcbe6ecdd4409e63ebfcf510
sarojpathak <sarojpathak430@gmail.com>
2019-12-09 21:51:27 +0545

    Initial commit

---
+++ app/src/main/java/com/saroj/recycleremailviewassignment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.saroj.recycleremailviewassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
